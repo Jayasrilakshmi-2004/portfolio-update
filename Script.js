7bc017a902e0d15868006a66ecc47352f273b842
Jayasrilakshmi-2004 <161324510+Jayasrilakshmi-2004@users.noreply.github.com>
2024-04-24 10:28:21 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,31 @@
+// Back to top section.
+const backTOTop = document.querySelector('.back-to-top');
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        backTOTop.classList.add("back-to-top-active");
+    } else {
+        backTOTop.classList.remove("back-to-top-active");
+    }
+});
+
+//hamburger
+
+const hamBurger = document.querySelector(".ham");
+const navLinks = document.querySelector(".nav-links");
+
+hamBurger.addEventListener("click", () => {
+    navLinks.classList.toggle("nav-links-active");
+    hamBurger.classList.toggle("ham-active");
+});
+
+
+// Preloader
+
+const preloader = document.querySelector(".loader");
+
+window.addEventListener("load", () => {
+    const fadeOut = setTimeout(() => {
+        preloader.classList.add("loader-active")
+    }, 2000);
+});
